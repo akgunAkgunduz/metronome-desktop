c1da7d87c221e0578c5ca8d1ac18fd1dac5d59d9
Akgün Akgündüz <akgun.akgunduz@gmail.com>
2018-09-17 23:16:22 +0300

    Change trainer UI

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function createWindow () {
   
   mainWindow = new BrowserWindow({
     width: 400, 
-    height: 310,
+    height: 342,
     x: mainWindowState.x,
     y: mainWindowState.y,
     frame: false,
